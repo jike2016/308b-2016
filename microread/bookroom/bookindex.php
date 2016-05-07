ad14e6b3d4ecb07976c384a1f972c21ef52ae077
jike2016 <jk12073002013@163.com>
2016-05-07 21:34:47 +0800

    微阅》书库》查询
    忘记勾选了

--- microread/bookroom/bookindex.php
+++ microread/bookroom/bookindex.php
@@ -1,7 +1,7 @@
 <?php
 require_once ("../../config.php");
 
-if(isset($_GET["bookid"]) && $_GET["bookid"] != null){//顶级分类
+if(isset($_GET["bookid"]) && $_GET["bookid"] != null){//阅读的书籍id
 	$bookid = $_GET["bookid"];
 }
 
@@ -9,13 +9,37 @@ require_once ("../loglib.php");
 addbookviewlog($bookid);//添加日志记录
 
 global $DB;
+global $USER;
+//查找阅读历史
+$readHistory = $DB->get_record_sql("select * from mdl_ebook_user_read_my eu
+									where eu.userid = $USER->id and eu.ebookid = $bookid ");
+if($readHistory){
+	$readStr = '继续阅读';
+}else{
+	$readStr = '在线阅读';
+}
 
 $bookclasses = $DB->get_records_sql("select e.id,e.name from mdl_ebook_categories_my e where e.parent = 0");//获取顶级分类
 //查询书籍信息
-$book = $DB->get_record_sql("select e.*,ea.`name` as authorname,ec.id,ec.`name` as categoryname from mdl_ebook_my e
+$book = $DB->get_record_sql("select e.*,ea.`name` as authorname,ec.id as bookclassid,ec.`name` as categoryname from mdl_ebook_my e
 								left join mdl_ebook_author_my ea on e.authorid = ea.id
 								left join mdl_ebook_categories_my ec on e.categoryid = ec.id
 								where e.id = $bookid");
+//查询书籍的所属顶级分类
+$booktopclass = $DB->get_record_sql("select ec2.* from mdl_ebook_categories_my ec1
+									left join mdl_ebook_categories_my ec2 on ec1.parent = ec2.id
+									where ec1.id = $book->bookclassid");
+if($booktopclass->name != null){//如果有顶级分类
+	$bookclasslist = $booktopclass->name.'&nbsp;>&nbsp;'.$book->categoryname;
+}else{//如果当前分类已经是顶级分类
+	$bookclasslist = $book->categoryname;
+}
+
+//获取书籍的标签
+$tags = $DB->get_records_sql("select tm.id,tm.tagname from mdl_tag_link tl
+								left join mdl_tag_my tm on tl.tagid = tm.id
+								where tl.link_id = $book->id");
+
 //查询各章节
 $bookchapters = $DB->get_records_sql("select * from mdl_ebook_chapter_my e
 										where e.ebookid = $bookid
@@ -33,58 +57,77 @@ $bookchapters = $DB->get_records_sql("select * from mdl_ebook_chapter_my e
 
 		<script type="text/javascript" src="../js/jquery-1.11.3.min.js" ></script>
 		<script type="text/javascript" src="../js/bootstrap.min.js" ></script>
+
+		<script>
+			//搜索选项下拉框
+			$(document).ready(function() {
+				$('#searchtype a').click(function() {
+					$('#searchtypebtn').text($(this).text());
+				});
+			});
+			//搜索
+			function search(){
+				var searchType = document.getElementById("searchtypebtn");//获取查询参数
+				var searchParam = document.getElementById("searchParam");//获取选项
+				window.location.href="searchresult.php?searchType="+searchType.textContent+"&searchParam="+searchParam.value;
+			}
+
+		</script>
+
 	</head>
 
 	<body id="bookindex">
-		<!--顶部导航-->
-		<div class="header">
-			<div class="header-center">
-				<a class="frist" href="<?php echo $CFG->wwwroot; ?>">首页</a>
-				<a href="<?php echo $CFG->wwwroot; ?>/mod/forum/view.php?id=1">微阅</a>
-				<a href="<?php echo $CFG->wwwroot; ?>/course/index.php">微课</a>
-				<a href="<?php echo $CFG->wwwroot; ?>/privatecenter/index.php?class=zhibo">直播</a>
-				<a class="login" href="#"><img src="../img/denglu.png"></a>
-			</div>
+	<!--顶部导航-->
+	<div class="header">
+		<div class="header-center">
+			<a class="frist" href="<?php echo $CFG->wwwroot; ?>">首页</a>
+			<a href="<?php echo $CFG->wwwroot; ?>/mod/forum/view.php?id=1">微阅</a>
+			<a href="<?php echo $CFG->wwwroot; ?>/course/index.php">微课</a>
+			<a href="<?php echo $CFG->wwwroot; ?>/privatecenter/index.php?class=zhibo">直播</a>
+			<a class="login" href="#"><img src="../img/denglu.png"></a>
 		</div>
-		
-		<div class="header-banner">
-			<a href="index.php"><img  src="../img/shuku_logo.png"/></a>
-			<!--搜索框组-->
-			<div class="search-box">
-				<div class="input-group">
-			     	<div class="input-group-btn">
-			        	<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">图书&nbsp;<span class="caret"></span></button>
-			        	<ul class="dropdown-menu">
-			          		<li><a href="#">图书</a></li>
-			          		<li role="separator" class="divider"></li>
-			          		<li><a href="#">文献</a></li>
-			          		<li role="separator" class="divider"></li>
-			          		<li><a href="#">论文</a></li>
-			        	</ul>
-			      	</div><!-- /btn-group -->
-			      	<input type="text" class="form-control" >
-			    </div><!-- /input-group -->
-			    <button class="btn btn-default searchbtn"><span class="glyphicon glyphicon-search"></span>&nbsp;搜索</button>
-			    
-			    <div class="radio">
-			  		<label>
-			    		<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
-			    		全部字段
-			  		</label>
-			  		<label>
-			    		<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-			    		标题
-			  		</label>
-			  		<label>
-			    		<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
-			    		主讲人
-			  		</label>
-				</div>
-			    
-			</div>
-			<!--搜索框组 end-->
+	</div>
+
+	<div class="header-banner">
+		<a href="index.php"><img  src="../img/shuku_logo.png"/></a>
+		<!--搜索框组-->
+		<div class="search-box">
+			<div class="input-group">
+				<div class="input-group-btn">
+					<button type="button" id="searchtypebtn" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">全部<span class="caret"></span></button>
+					<ul id="searchtype" class="dropdown-menu">
+						<li><a id="bookall" href="#">全部</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a id="booktitle" href="#">标题</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a id="bookauthor" href="#">作者</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a id="bookuploader" href="#">上传者</a></li>
+					</ul>
+				</div><!-- /btn-group -->
+				<input id="searchParam" type="text" class="form-control" >
+			</div><!-- /input-group -->
+			<button onclick="search()" class="btn btn-default searchbtn"><span class="glyphicon glyphicon-search"></span>&nbsp;搜索</button>
+
+			<!--			    <div class="radio">-->
+			<!--			  		<label>-->
+			<!--			    		<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">-->
+			<!--			    		全部字段-->
+			<!--			  		</label>-->
+			<!--			  		<label>-->
+			<!--			    		<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">-->
+			<!--			    		标题-->
+			<!--			  		</label>-->
+			<!--			  		<label>-->
+			<!--			    		<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">-->
+			<!--			    		主讲人-->
+			<!--			  		</label>-->
+			<!--				</div>-->
+
 		</div>
-		<!--顶部导航 end-->
+		<!--搜索框组 end-->
+	</div>
+	<!--顶部导航 end-->
 		
 		<!--书本分类-->
 		<div class="bookclassified">
@@ -133,9 +176,26 @@ $bookchapters = $DB->get_records_sql("select * from mdl_ebook_chapter_my e
 				<div class="bookinfo">
 					<p class="titleword">作者：&nbsp;&nbsp;</p><p class="titleinfo"><?php echo $book->authorname; ?></p><br />
 					<p class="titleword">简介：&nbsp;&nbsp;</p><p class="titleinfo"><?php echo $book->summary; ?></p><br />
-					<p class="titleword">所属分类：&nbsp;&nbsp;</p><a href="#" class="classify" ><?php echo $book->categoryname; ?></a><br />
+					<p class="titleword">所属分类：&nbsp;&nbsp;</p>
+						<?php
+							if($booktopclass->name != null) {//如果是二级分类，则会有顶级分类
+								echo '<a href="classify.php?bookclassid='.$booktopclass->id .'";  class="classify" >'.$booktopclass->name.'</a> &nbsp;>&nbsp;';
+								echo '<a href="classify.php?bookclassid='.$booktopclass->id  .'&booksecondclassid='.$book->bookclassid.'";  class="classify" >'.$book->categoryname.'</a>';
+							}else{//如果是顶级分类
+								echo '<a href="classify.php?bookclassid='.$book->bookclassid.'" class="classify" >'.$book->categoryname.'</a>';
+							}
+						?>
+					<br />
+					<p class="titleword">字数：&nbsp;&nbsp;</p><?php echo $book->wordcount; ?><br />
+					<p class="titleword">标签：&nbsp;&nbsp;</p>
+					<?php
+						foreach($tags as $tag){
+							echo '<a class="tips">'.$tag->tagname.'</a>&nbsp;&nbsp;';
+						}
+					?>
+					<br />
 					<div class="btnbox">
-						<a href="#" class="functionbtn">在线阅读</a>
+						<a href="onlineread.php?bookid=<?php echo $book->id; ?>" target="_blank" class="functionbtn"><?php echo $readStr; ?></a>
 						<a href="<?php echo $book->url; ?>" download="" class="functionbtn">下载此书</a>
 					</div>
 				</div>
@@ -158,7 +218,7 @@ $bookchapters = $DB->get_records_sql("select * from mdl_ebook_chapter_my e
 									<a href="#">'.$bookchapter->name.'</a>
 									<ul>';
 						foreach($booksections as $booksection){
-							echo '<li><a href="#">'.$booksection->name.'</a></li>';
+							echo '<li><a href="onlineread.php?bookid='.$bookid.'&booksectionid='.$booksection->id.'" target="_blank">'.$booksection->name.'</a></li>';
 						}
 						echo '</ul>
 							</p>';
---
+++ microread/bookroom/searchresult.php
@@ -0,0 +1,272 @@
+<?php
+require_once ("../../config.php");
+
+$page = optional_param('page', 1, PARAM_INT);//分页页号
+
+if(isset($_GET["searchType"]) && $_GET["searchType"] != null){//搜索类型
+	$searchType = $_GET["searchType"];
+}
+if(isset($_GET["searchParam"]) && $_GET["searchParam"] != null){//搜索参数
+	$searchParam = $_GET["searchParam"];
+}
+
+global $DB;
+$bookclasses = $DB->get_records_sql("select e.id,e.name from mdl_ebook_categories_my e where e.parent = 0");//获取顶级分类
+
+switch($searchType){
+	case '全部':
+		$sql = "where em.name like '%$searchParam%' or ea.name like '%$searchParam%' or em.summary like '%$searchParam%'";
+		break;
+	case '标题':
+		$sql = "where em.name like '%$searchParam%'";
+		break;
+	case '作者':
+		$sql = "where ea.name like '%$searchParam%'";
+		break;
+	case '上传者':
+		$sql = "";
+		break;
+	default:
+		break;
+}
+
+//查询结果
+$index = ($page-1)*12;//从第几条记录开始
+$searchResults = $DB->get_records_sql("select em.*,ea.name as authorname from mdl_ebook_my em
+								left join mdl_ebook_author_my ea on em.authorid = ea.id
+								$sql
+								order by em.timecreated desc
+								limit $index,10 ");
+
+//如果还没有设置过查询结果的数量
+if(isset($_POST["searchcount"])){
+	$searchcount = $_POST['searchcount'];
+}else{
+	$searchResultsCount = $DB->get_records_sql("select em.*,ea.name as authorname from mdl_ebook_my em
+								left join mdl_ebook_author_my ea on em.authorid = ea.id
+								$sql
+								order by em.timecreated desc");
+	$searchcount = count($searchResultsCount);
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>书库搜索结果页</title>
+		<link rel="stylesheet" href="../css/bootstrap.css" />
+		<link rel="stylesheet" href="../css/bookroom_searchresult.css" />
+
+		<script type="text/javascript" src="../js/jquery-1.11.3.min.js" ></script>
+		<script type="text/javascript" src="../js/bootstrap.min.js" ></script>
+
+		<script>
+			//搜索选项下拉框
+			$(document).ready(function() {
+				$('#searchtype a').click(function() {
+					$('#searchtypebtn').text($(this).text());
+				});
+			});
+			//搜索
+			function search(){
+				var searchType = document.getElementById("searchtypebtn");//获取查询参数
+				var searchParam = document.getElementById("searchParam");//获取选项
+				window.location.href="searchresult.php?searchType="+searchType.textContent+"&searchParam="+searchParam.value;
+			}
+
+		</script>
+
+	</head>
+	<body id="bookroom_searchresult">
+
+		<form id="pagerForm" method="post" action="">
+			<input type="hidden" name="searchcount" value="<?php echo $searchcount;?>" />
+		</form>
+
+		<!--顶部导航-->
+		<div class="header">
+			<div class="header-center">
+				<a class="frist" href="<?php echo $CFG->wwwroot; ?>">首页</a>
+				<a href="<?php echo $CFG->wwwroot; ?>/mod/forum/view.php?id=1">微阅</a>
+				<a href="<?php echo $CFG->wwwroot; ?>/course/index.php">微课</a>
+				<a href="<?php echo $CFG->wwwroot; ?>/privatecenter/index.php?class=zhibo">直播</a>
+				<a class="login" href="#"><img src="../img/denglu.png"></a>
+			</div>
+		</div>
+		
+		<div class="header-banner">
+			<a href="index.php"><img  src="../img/shuku_logo.png"/></a>
+			<!--搜索框组-->
+			<div class="search-box">
+				<div class="input-group">
+					<div class="input-group-btn">
+						<button type="button" id="searchtypebtn" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">全部<span class="caret"></span></button>
+						<ul id="searchtype" class="dropdown-menu">
+							<li><a id="bookall" href="#">全部</a></li>
+							<li role="separator" class="divider"></li>
+							<li><a id="booktitle" href="#">标题</a></li>
+							<li role="separator" class="divider"></li>
+							<li><a id="bookauthor" href="#">作者</a></li>
+							<li role="separator" class="divider"></li>
+							<li><a id="bookuploader" href="#">上传者</a></li>
+						</ul>
+					</div><!-- /btn-group -->
+					<input id="searchParam" type="text" class="form-control" >
+			    </div><!-- /input-group -->
+			    <button onclick="search()" class="btn btn-default searchbtn"><span class="glyphicon glyphicon-search"></span>&nbsp;搜索</button>
+			    
+<!--			    <div class="radio">-->
+<!--			  		<label>-->
+<!--			    		<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">-->
+<!--			    		全部字段-->
+<!--			  		</label>-->
+<!--			  		<label>-->
+<!--			    		<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">-->
+<!--			    		标题-->
+<!--			  		</label>-->
+<!--			  		<label>-->
+<!--			    		<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">-->
+<!--			    		主讲人-->
+<!--			  		</label>-->
+<!--				</div>-->
+			    
+			</div>
+			<!--搜索框组 end-->
+		</div>
+		<!--顶部导航 end-->
+		
+		<!--书本分类-->
+		<div class="bookclassified">
+			<div class="bookclassified-center">
+				
+				<!-- 书本分类按钮 -->
+<!--				<div class="btn-group" style="float: left;">-->
+<!--				  	<a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">-->
+<!--				  		<img src="../img/tushuFenlei.png">-->
+<!--				  	</a>-->
+<!--				  	<ul class="dropdown-menu">-->
+<!--				    	<li><a href="#">现代</a></li>-->
+<!--				    	<li role="separator" class="divider"></li>-->
+<!--				    	<li><a href="#">军事</a></li>-->
+<!--				    	<li role="separator" class="divider"></li>-->
+<!--				    	<li><a href="#">战争</a></li>-->
+<!--				    	<li role="separator" class="divider"></li>-->
+<!--				    	<li><a href="#">科技</a></li>-->
+<!--				  	</ul>-->
+<!--				</div>-->
+				<!-- 书本分类按钮 end-->
+
+				<?php
+					if($bookclasses != null){
+						foreach($bookclasses as $bookclass){
+							echo '<div class="line"></div>
+									<a href="classify.php?bookclassid='.$bookclass->id.'" class="kinds">'.$bookclass->name.'</a>';
+						}
+					}
+				?>
+			</div>
+		</div>
+		<!--书本分类 end-->
+		
+		<!--页面主体-->
+		<div class="main">
+			<?php
+				foreach($searchResults as $searchResult){
+					echo '<div class="book-block">
+								<img src="'.$searchResult->pictrueurl.'" width="105" height="150"/>
+								<div class="book-info-box">
+									<a href="bookindex.php?bookid='.$searchResult->id.'" target="_blank"><p class="bookname">'.$searchResult->name.'</p></a>
+									<p class="writer">作者：'.$searchResult->authorname.'</p>
+									<p class="bookinfo">'.$searchResult->summary.'</p>
+									<p>';
+
+					//获取书籍的标签
+					$tags = $DB->get_records_sql("select tm.id,tm.tagname from mdl_tag_link tl
+								left join mdl_tag_my tm on tl.tagid = tm.id
+								where tl.link_id = $searchResult->id");
+					foreach($tags as $tag){
+						echo '<a class="tips">'.$tag->tagname.'</a>';
+					}
+					echo '			</p>
+								</div>
+							</div>';
+				}
+			?>
+		</div>
+		<!--页面主体 end-->
+
+		<!--分页-->
+		<div style="clear: both;"></div>
+		<div class="paging" style="text-align: center;">
+			<nav>
+				<ul class="pagination">
+					<li>
+						<a href="searchresult.php?searchType=<?php echo $searchType; ?>&searchParam=<?php echo $searchParam; ?>">
+							<span aria-hidden="true">首页</span>
+						</a>
+					</li>
+					<li>
+						<?php
+						if(($page-1)<= 0){
+							$prepage = 1;
+						}else{
+							$prepage = $page-1;
+						}
+						?>
+						<a href="searchresult.php?searchType=<?php echo $searchType; ?>&searchParam=<?php echo $searchParam; ?>&page=<?php echo $prepage; ?>" aria-label="Previous">
+							<span aria-hidden="true">上一页</span>
+						</a>
+					</li>
+					<?php
+					$totalpage = ceil($searchcount/12);
+					for($i=1;$i<=$totalpage;$i++){
+						echo ' <li><a href="searchresult.php?searchType='.$searchType.'&searchParam='.$searchParam.'&page='.$i.'">'.$i.'</a></li>';
+					}
+					?>
+
+					<li>
+						<?php
+						if(($page+1)>= ceil($searchcount/12) ){
+							$nextpage = ceil($searchcount/12);
+						}else{
+							$nextpage = $page+1;
+						}
+						?>
+						<a href="searchresult.php?searchType=<?php echo $searchType; ?>&searchParam=<?php echo $searchParam; ?>&page=<?php echo $nextpage; ?>" aria-label="Next">
+							<span aria-hidden="true">下一页</span>
+						</a>
+					</li>
+					<li>
+						<a href="searchresult.php?searchType=<?php echo $searchType; ?>&searchParam=<?php echo $searchParam; ?>&page=<?php echo ceil($searchcount/12); ?>">
+							<span aria-hidden="true">尾页</span>
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+		<!--分页 end-->
+		
+		<div style="clear: both;"></div>
+		<!--底部导航条-->
+		<nav class="bottomnav">
+			<div class="whiteline"></div>
+			<p>
+				<a>电子书友链</a>
+				<a>QQ：54250413230</a>
+				<a>版权声明</a>
+				<a>意见反馈</a>
+				<a>客服电话（0771-536780）</a>
+			</p>
+			<p>
+				<a>单位编号：1101081827</a>
+				<a>防城慕课网</a>
+				<a>桂ICP证：060172号</a>
+				<a>网络视听许可证：0110438号</a>
+			</p>
+			<p>Copyright &nbsp;1999-2012&nbsp;&nbsp;防城慕课</p>
+		</nav>
+		<!--底部导航条 end-->
+	</body>
+</html>

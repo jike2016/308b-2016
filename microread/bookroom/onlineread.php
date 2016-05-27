c9bab4f59feb00a7b940549eb80b2ba3fbbe40b7
jike2016 <jk12073002013@163.com>
2016-05-27 18:06:47 +0800

    微阅》书库文本阅读调整

--- microread/bookroom/onlineread.php
+++ microread/bookroom/onlineread.php
@@ -114,9 +114,7 @@ if($readHistory == null) {
 }else{
 	$updateHistory = $DB->update_record("ebook_user_read_my", array('id'=>$readHistory->id, 'sectionid'=>$showSection->id,'timecreated'=>time()));
 }
-
 ?>
-
 <!DOCTYPE html>
 <html>
 	<head>
@@ -137,9 +135,93 @@ if($readHistory == null) {
 		<script type="text/javascript" src="../js/jquery-1.11.3.min.js" ></script>
 		<script type="text/javascript" src="../js/bootstrap.min.js" ></script>
 		<script type="text/javascript" src="../js/pdfobject.min.js" ></script>
+		<script>
+			var Get_color = '#FFFFFF';
+			var Get_fsize = '24px';
+			var Get_fcolor = '#000';
+			var Get_fontfamily = '微软雅黑';
+		</script>
+		<?php
+			if(isset($_GET["color"]) && $_GET["color"]!= null){//文字背景色
+				$backcolor = $_GET["color"];
+				echo "<script>Get_color = '$backcolor';</script>";
+			}
+			if(isset($_GET['fsize']) && $_GET['fsize']!= null){//文字大小
+				$fontsize = $_GET['fsize'];
+				echo "<script>Get_fsize = '$fontsize'; </script>";
+			}
+			if(isset($_GET['fcolor']) && $_GET['fcolor']!= null){//文字颜色
+				$fontcolor = $_GET['fcolor'];
+				echo "<script>Get_fcolor = '$fontcolor'; </script>";
+			}
+			if(isset($_GET['fontfamily']) && $_GET['fontfamily']!= null){//文字字形
+				$fontfamily = $_GET['fontfamily'];
+				echo "<script>Get_fontfamily = '$fontfamily'; </script>";
+			}
+		?>
+
+		<!--Start 在线阅读文字，设置背景色、字体 -->
+		<script>
+			$(document).ready(function(){
+
+				//初始化，保留之前页面字体的样式设置
+				if( Get_color != '' ){
+					//设置选项的选中值
+					$("#backcolor").val(Get_color);
+					$("#fontsize").val(Get_fsize);
+					$("#fontcolor").val(Get_fcolor);
+					$("#fontfamily").val(Get_fontfamily);
+					//调整显示样式
+					$('.book-box').css("background-color",Get_color);
+					$('.article *').css("font-size",Get_fsize);
+					$('.article *').css("color",Get_fcolor);
+					$('.article *').css("font-family",Get_fontfamily);
+				}
+				//end 初始化，保留之前页面字体的样式设置
+
+				//切换字体样式
+				$('#backcolor').change(function(){
+					var color = $(this).val();
+					$('.book-box').css("background-color",color);
+				});
+				$('#fontsize').change(function(){
+					var fsize = $(this).val();
+					$('.article *').css("font-size",fsize);
+				});
+				$('#fontcolor').change(function(){
+					var fcolor = $(this).val();
+					$('.article *').css("color",fcolor);
+				});
+				$('#fontfamily').change(function(){
+					var fontfamily = $(this).val();
+					$('.article *').css("font-family",fontfamily);
+				});
+				//end 切换字体样式
+
+			});
+
+			//上下节的跳转
+			function  next_page(bookid,sectionid,booktype){
+
+				//注意判断章节中间是pdf 的情况
+				if(booktype==1){//如果输txt 格式
+					Get_color = $("#backcolor option:selected").val();
+					Get_fsize = $("#fontsize option:selected").val();
+					Get_fcolor = $("#fontcolor option:selected").val();
+					Get_fontfamily = $("#fontfamily option:selected").val();
+					var newhref = "onlineread.php?bookid="+ bookid +"&booksectionid="+sectionid+"&color="+Get_color+"&fsize="+Get_fsize+"&fcolor="+Get_fcolor+"&fontfamily="+Get_fontfamily;
+				}else{//如果是pdf 等其他格式
+					var newhref = "onlineread.php?bookid="+ bookid +"&booksectionid="+sectionid;
+				}
+//				alert(newhref);
+				window.location.href = newhref;
+			}
+		</script>
+		<!--End 在线阅读文字，设置背景色、字体 -->
 
 	</head>
 	<body id="bookroom_readonline">
+
 		<!--顶部导航-->
 		<?php
 			require_once ("../common/book_head_login.php");//微阅登录导航栏：首页、微阅、微课、、、、
@@ -183,21 +265,78 @@ if($readHistory == null) {
 		<!--页面主体-->
 		<div class="main">
 			<div class="book-box">
-				<div class="head-info">
+				<div class="headback">
+					<div class="head-info">
+						<?php
+							if($booktopclass->name != null) {//如果有顶级分类
+								echo '<a href="#">'.$booktopclass->name.'</a>
+										<span>&nbsp;>&nbsp;</span>';
+							}
+						?>
+						<a href="#"><?php echo $book->categoryname; ?></a>
+						<span>&nbsp;>&nbsp;</span>
+						<a href="#"><?php echo $book->name; ?></a>
+						<span>&nbsp;>&nbsp;</span>
+						<a href="#" class="chaptertitle"><?php echo $showchapter->name; ?></a>
+					</div>
+
 					<?php
-						if($booktopclass->name != null) {//如果有顶级分类
-							echo '<a href="#">'.$booktopclass->name.'</a>
-									<span>&nbsp;>&nbsp;</span>';
+						if($showSection->type == 1){//如果是 txt 文本，则显示文字样式控制
+							echo '<!--新加文本选项-->
+									<div class="head-option">
+										<!--背景颜色-->
+										<span>背景颜色：</span>
+										<select id="backcolor" class="form-control">
+											<option  value ="#FFFFFF">白色</option>
+											<option style="background-color: #F0f0f0;" value ="#F0f0f0">灰色</option>
+											<option style="background-color: #CCC;" value ="#CCC">深灰</option>
+											<option style="background-color: #f9b3b6;" value ="#f9b3b6">红色</option>
+											<option style="background-color: rgb(255, 255, 237);" value="rgb(255, 255, 237)">黄色</option>
+											<option style="background-color: rgb(238, 250, 238);" value="rgb(238, 250, 238)">绿色</option>
+											<option style="background-color: rgb(238, 250, 238);" value="rgb(230, 243, 255)">蓝色</option>
+										</select>
+										<!--背景颜色 end-->
+
+										<!--字体大小-->
+										<span>字体大小：</span>
+										<select id="fontsize" class="form-control">
+											<option value ="24px">24px</option>
+											<option value ="30px">30px</option>
+											<option value="16px">16px</option>
+											<option value="14px">14px</option>
+										</select>
+										<!--字体大小 end-->
+
+										<!--字体颜色-->
+										<span>字体颜色：</span>
+										<select id="fontcolor" class="form-control">
+											<option value ="#000">黑色</option>
+											<option style="color: #FFFFFF; background-color:#555;" value ="#FFFFFF">白色</option>
+											<option style="color: #F0776C;" value ="#F0776C">红色</option>
+											<option style="color: rgb(0, 102, 0);" value="rgb(0, 102, 0)">绿色</option>
+											<option style="color: rgb(0, 0, 255);" value="rgb(0, 0, 255)">蓝色</option>
+										</select>
+										<!--字体颜色 end-->
+
+										<!--字体类型-->
+										<span>字体类型：</span>
+										<select id="fontfamily" class="form-control">
+											<option value ="微软雅黑">微软雅黑</option>
+											<option style="font-family: 宋体;"  value ="宋体">宋体</option>
+											<option style="font-family: 楷体;" value="楷体">楷体</option>
+											<option style="font-family: 黑体;" value="黑体">黑体</option>
+										</select>
+										<!--字体类型 end-->
+									</div>
+									<!--新加文本选项 end-->';
 						}
 					?>
-					<a href="#"><?php echo $book->categoryname; ?></a>
-					<span>&nbsp;>&nbsp;</span>
-					<a href="#"><?php echo $book->name; ?></a>
-					<span>&nbsp;>&nbsp;</span>
-					<a href="#" class="chaptertitle"><?php echo $showchapter->name; ?></a>
+					<div style="clear: both;"></div>
 				</div>
+
 				<p class="title"><?php echo $showSection->name; ?></p>
 				<div id="pdf" class="article">
+					<p>
 					<?php
 						if($showSection->type == 1){//文本格式
 							echo $showSection->text;
@@ -205,11 +344,12 @@ if($readHistory == null) {
 //							 echo $showSection->pdfurl;
 						}
 					?>
+					</p>
 				</div>
 				<div class="bottom-info">
-					<a href="onlineread.php?bookid=<?php echo $bookid; ?>&booksectionid=<?php echo $lastsectionid; ?>">上一页</a>&nbsp;&nbsp;&nbsp;&nbsp;
+					<a href="#" onclick="next_page('<?php echo $bookid; ?>','<?php echo $lastsectionid; ?>','<?php echo $showSection->type; ?>')" class="" >上一页</a>&nbsp;&nbsp;&nbsp;&nbsp;
 					<a href="bookindex.php?bookid=<?php echo $bookid; ?>">章节目录</a>&nbsp;&nbsp;&nbsp;&nbsp;
-					<a href="onlineread.php?bookid=<?php echo $bookid; ?>&booksectionid=<?php echo $nextsectionid; ?>">下一页</a>
+					<a href="#" onclick="next_page('<?php echo $bookid; ?>','<?php echo $nextsectionid; ?>','<?php echo $showSection->type; ?>')" class="" >下一页</a>
 				</div>
 			</div>
 		</div>

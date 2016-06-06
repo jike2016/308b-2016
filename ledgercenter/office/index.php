70eef696aa7680af8340601d1b2a5b8d2812c8f5
jike2016 <jk12073002013@163.com>
2016-06-06 12:17:01 +0800

    台账数据中心》更改查询条件
    1、将按周月总查询改为按照时间段查询
    2、其中只有学习任物按照时间查询，台账任务没有设定时间查询条件

--- ledgercenter/office/index.php
+++ ledgercenter/office/index.php
@@ -44,10 +44,14 @@ $output1 = '<input type="text" class="form-control classkinds" value="'.$firstmi
 		if(documentlist_second_son_id==1){//学习统计
 			document.getElementById("mytime").style.display="";
 			document.getElementById("mymission").style.display="none";
+			document.getElementById("start_time_content").style.display="";
+			document.getElementById("end_time_content").style.display="";
 		}
 		else{
 			document.getElementById("mytime").style.display="none";
 			document.getElementById("mymission").style.display="";
+			document.getElementById("start_time_content").style.display="none";
+			document.getElementById("end_time_content").style.display="none";
 		}
 	});
 	$('#documentlist-third-son li').on('click', function(){   //文档_子类型下拉表单 3动作
@@ -56,27 +60,54 @@ $output1 = '<input type="text" class="form-control classkinds" value="'.$firstmi
 	$('#documentlist-fourth-son li').on('click', function(){   //文档_子类型下拉表单 3动作
 		documentlist_fourth_son_id = $(this).val();       //获取文档_子类型下拉菜单 3选择项的id
 	});
-	
+
+	//Start 时间字符串转换为时间戳 dateStr = 2016-05-25 12:12:12 xdw
+	function get_unix_time(dateStr)
+	{
+		var newstr = dateStr.replace(/-/g,'/');
+		var date =  new Date(newstr);
+		var time_str = date.getTime().toString();
+		return time_str.substr(0, 10);
+	}
+	//end 时间字符串转换为时间戳 dateStr = 2016-05-25 12:12:12 xdw
 	
 	$(".search").on('click', function(){   //搜索按钮动作
 		$('.lockpage').show();
 		// var treenodeactiveid = $(".curSelectedNode").attr("id");  //获取激活的树节点的id	
-		 // alert(documentlist_fourth_son_id+'f');		
+		 // alert(documentlist_fourth_son_id+'f');
+
+		//Start 添加时间段 xdw
+		var start_time = $('#start_time').val();//开始时间
+		var end_time = $('#end_time').val();//结束时间
+		if(start_time=='' || end_time==''){
+			alert('开始、结束时间不能为空！');
+			return;
+		}
+		start_time = get_unix_time(start_time);
+		end_time = get_unix_time(end_time);
+		if(start_time >= end_time){
+			alert('结束时间不能小于开始时间！');
+			return;
+		}
+		//End 添加时间段 xdw
+
 		if(orgid==null){
 			alert('请选择组织架构');
 			$('.lockpage').hide();
 		}
 		else if(documentlist_first_son_id==1&&documentlist_second_son_id==1){
-			$(".table-box").load('office/officelearn.php?timeid='+documentlist_third_son_id+'&orgid='+orgid);	
+//			$(".table-box").load('office/officelearn.php?timeid='+documentlist_third_son_id+'&orgid='+orgid);
+			$(".table-box").load('office/officelearn.php?timeid='+documentlist_third_son_id+'&start_time='+start_time+'&end_time='+end_time+'&orgid='+orgid);
 		}
 		else if(documentlist_first_son_id==1&&documentlist_second_son_id==2){
 			$(".table-box").load('office/officemission.php?missionid='+documentlist_fourth_son_id+'&orgid='+orgid);
 		}
 		else if(documentlist_first_son_id==2&&documentlist_second_son_id==1){
-			$(".table-box").load('office/personlearn.php?timeid='+documentlist_third_son_id+'&orgid='+orgid);	
+//			$(".table-box").load('office/personlearn.php?timeid='+documentlist_third_son_id+'&orgid='+orgid);
+			$(".table-box").load('office/personlearn.php?timeid='+documentlist_third_son_id+'&start_time='+start_time+'&end_time='+end_time+'&orgid='+orgid);
 		}
 		else if(documentlist_first_son_id==2&&documentlist_second_son_id==2){
-			$(".table-box").load('office/personmission.php?missionid='+documentlist_fourth_son_id+'&orgid='+orgid);	
+			$(".table-box").load('office/personmission.php?missionid='+documentlist_fourth_son_id+'&orgid='+orgid);
 		}
 		
 		//alert("1:"+documentlist_first_son_id+"    2:"+documentlist_second_son_id+"   3:"+documentlist_third_son_id)
@@ -86,6 +117,47 @@ $output1 = '<input type="text" class="form-control classkinds" value="'.$firstmi
 		//alert("激活的树节点:  "+treenodeactiveid+"\r台账类型:  "+documentlistactive_id+"\r统计类型:  "+documentlist_first_son_id+"\r文档_子类型2:  "+documentlist_second_son_id+"\r文档_子类型3:  "+documentlist_third_son_id);
 	});
 </script>
+
+<!--Start 时间日期控件  xdw -->
+<link rel="stylesheet" href="../ledgercenter/css/jquery-ui.css" />
+<style type="text/css">
+	a{color:#007bc4/*#424242*/; text-decoration:none;}
+	a:hover{text-decoration:underline}
+	ol,ul{list-style:none}
+	body{font:12px/18px Tahoma, Helvetica, Arial, Verdana, "\5b8b\4f53", sans-serif; color:#51555C;}
+	img{border:none}
+	input{width:200px; height:20px; line-height:20px; padding:2px; border:1px solid #d3d3d3}
+	pre{padding:6px 0 0 0; color:#666; line-height:20px; background:#f7f7f7}
+
+	.ui-timepicker-div .ui-widget-header { margin-bottom: 8px;}
+	.ui-timepicker-div dl { text-align: left; }
+	.ui-timepicker-div dl dt { height: 25px; margin-bottom: -25px; }
+	.ui-timepicker-div dl dd { margin: 0 10px 10px 65px; }
+	.ui-timepicker-div td { font-size: 90%; }
+	.ui-tpicker-grid-label { background: none; border: none; margin: 0; padding: 0; }
+	.ui_tpicker_hour_label,.ui_tpicker_minute_label,.ui_tpicker_second_label,.ui_tpicker_millisec_label,.ui_tpicker_time_label{padding-left:20px}
+</style>
+<script type="text/javascript" >
+
+	jqmin(document).ready(function(){
+		jqmin('#start_time').datetimepicker({
+			showSecond: true,
+//			showMillisec: true,//显示毫秒
+			timeFormat: 'hh:mm:ss'
+		});
+		jqmin('#end_time').datetimepicker({
+			showSecond: true,
+//			showMillisec: true,//显示毫秒
+			timeFormat: 'hh:mm:ss'
+		});
+	});
+
+</script>
+<!--End 时间日期控件  xwd -->
+
+
+
+
 <div class="dropdownlist-box">
 			<!--文档_子类型下拉菜单 1-->
 	<div class="dropdownlist">
@@ -131,15 +203,13 @@ $output1 = '<input type="text" class="form-control classkinds" value="'.$firstmi
 		</div>
 	</div>
 	<!--文档_子类型下拉菜单 3end-->
-	<!--文档_子类型下拉菜单 3-->
+
 	<div class="dropdownlist" id="mymission">
 		<div class="input-group">
 			
 				<?php 
-				
-				echo $output1;
-				echo $output2;
-				
+					echo $output1;
+					echo $output2;
 				?>
 					
 				</ul>
@@ -147,6 +217,21 @@ $output1 = '<input type="text" class="form-control classkinds" value="'.$firstmi
 		</div>
 	</div>
 	<!--文档_子类型下拉菜单 3end-->
+
+<!--Start 添加时间日期控件 xwd-->
+<div class="dropdownlist" id="start_time_content">
+	<div class="input-group">
+		<div>开始时间：</div>
+		<div><input type="text" id="start_time" /></div>
+	</div>
+</div>
+<div class="dropdownlist" id="end_time_content">
+	<div class="input-group">
+		<div>结束时间：</div>
+		<div><input type="text" id="end_time" /></div>
+	</div>
+</div>
+<!--End 添加时间日期控件 xwd-->
 	
 
 	<button class="btn btn-primary search">搜索</button>
